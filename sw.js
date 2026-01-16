76aa5be8363149ded10c26ff7444794d5b7036fc
prasadanand412 <prasad.anand412@gmail.com>
2026-01-16 09:25:38 +0530

    Added a PWA

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "pwa-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/styles.css",
+  "/script.js",
+  "/logo-192.png",
+  "/logo-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
